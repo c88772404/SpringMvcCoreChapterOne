39263a6f404774c5350359b563d5a5786ca4a1d0
c88772404 <c88772404@gmail.com>
2023-09-12 03:43:39 +0900

    section 1,2

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -22,7 +22,7 @@ public class RequestHeaderServlet extends HttpServlet {
     }
 
     private static void printStartLine(HttpServletRequest request) {
-        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("--- REQUEST-LINE - start ----");
 
         System.out.println("request.getMethod() = " + request.getMethod()); //GET
         System.out.println("request.getProtocal() = " + request.getProtocol()); //HTTP/1.1
